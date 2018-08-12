9121906c233c8b03a477178a8bf0ea35e268176a
n8659150 <jackli900421@hotmail.com>
2018-08-12 21:27:51 +0800

    删除不必要的代码

--- puppeteer_crawler.js
+++ puppeteer_crawler.js
@@ -5,26 +5,8 @@ const maxScrollCount = 10;
 let wait = (ms = 1000) => new Promise(resolve => setTimeout(() => resolve(), ms));
 
 (async () => {
-  // const browser = await puppeteer.launch({ headless: false })
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
-  // // 登录部分
-  // await page.goto('http://zhihu.com');
-  // // 视口宽度默认800*600，不设置视口宽度的话 无法点击页面右侧“登录”按钮
-  // await page.setViewport({
-  //   width: 1920,
-  //   height: 1080
-  // });
-  // // 未登录状态下的右侧“登录”按钮
-  // await page.click('button.Button.HomeSidebar-signBannerButton.Button--blue.Button--spread');
-  // // 输入用户名密码模拟登录
-  // await page.type('input[name="username"]', 'xxxxxx', {delay: 300});
-  // await page.type('input[name="password"]', 'xxxxxx', {delay: 300});
-  // await page.keyboard.down('Enter');
-  // await page.waitForNavigation()
-  // // 登录部分结束
-
   // 爬虫执行部分
   await page.goto('https://www.zhihu.com/question/61235373');
   console.log(`page reached`);
@@ -56,6 +38,5 @@ let wait = (ms = 1000) => new Promise(resolve => setTimeout(() => resolve(), ms)
   console.log(targetLink);
   console.log(`grabbed ${targetLink.length} pics`);
   await browser.close();
-
 })()
 
