79a18b6e841f7e6119e941a1feacce66a87044a9
n8659150 <jackli900421@hotmail.com>
2018-08-14 22:55:51 +0800

    抓图接口新增页数选择参数，新增获取问题的接口

--- puppeteer_crawler.js
+++ puppeteer_crawler.js
@@ -2,23 +2,42 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
 // 下拉加载图片的次数 (别太多不然营养跟不上:))
-const maxScrollCount = 10;
+// const maxScrollCount = 10;
 // 等待下拉加载完成，默认1000ms,网络情况好可缩短,不佳可延长。
 let wait = (ms = 1000) => new Promise(resolve => setTimeout(() => resolve(), ms));
 
-//api /sister?questionId=88888888
+//api /sisterTitle?questionId=88888888
+app.get('/sisterTitle', async (req, res) => {
+  const browser = await puppeteer.launch();
+  console.log('launched');
+  // const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  console.log('new page created');
 
-app.get('/sister', async (req, res) => {
+  // 爬虫执行部分
+  let questionId = req.query.questionId;
+  let URL = `https://www.zhihu.com/question/${questionId}`;
+  await page.goto(URL,{timeout:2000});
+  console.log(`page reached`);
+  let pageTitle = await page.title();
+  await browser.close();
+  res.json(pageTitle);
+})
+
+//api /sisterImg?questionId=88888888
+
+app.get('/sisterImg', async (req, res) => {
     const browser = await puppeteer.launch()
     console.log('launched');
     // const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    console.log('new page created')
+    console.log('new page created');
   
     // 爬虫执行部分
     let questionId = req.query.questionId;
+    let pageCount = Number(req.query.pageCount);
     let URL = `https://www.zhihu.com/question/${questionId}`;
-    await page.goto(URL,{timeout:2000});
+    await page.goto(URL,{timeout:6000});
     console.log(`page reached`);
     // await wait(2000);
     await page.setViewport({
@@ -31,7 +50,8 @@ app.get('/sister', async (req, res) => {
       console.log(`'load all' button clicked`);
     }
     // 下拉加载，并且等待加载完成，重复n次
-    for (let i = 0; i < maxScrollCount; i++) {
+    console.log(`page count is ${pageCount}`)
+    for (let i = 0; i <= pageCount; i++) {
       console.log(`loading page ${i}`);
       await page.evaluate(() => {
         window.scrollTo(0, document.body.scrollHeight);
