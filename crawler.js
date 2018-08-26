3fd640a2c8b7745b3adffd2fa5f78a7d986162c3
n8659150 <jackli900421@hotmail.com>
2018-08-26 14:23:31 +0800

    新增查询标题的api，并加入errorhandler

--- crawler.js
+++ crawler.js
@@ -1,13 +1,13 @@
 let fs = require('fs');
 let rp = require('request-promise');
 class zhihuCrawler {
-  
+
   constructor(questionId) {
     this.isEnd = false;
     this.questionId = questionId;
   }
-
-  async init(offset, limit) {
+  
+  async init(offset = 0, limit = 100) {
     if (this.isEnd) {
       console.log('到底了');
       return
@@ -20,11 +20,22 @@ class zhihuCrawler {
     return { 'meta': question, 'imgSrc': imgs, 'total': imgs.length }
   }
 
+  async getQuestion() {
+    let uri = `https://www.zhihu.com/api/v4/questions/${this.questionId}`;
+    let response = {};
+    try {
+      response = await rp({ uri, json: true, timeout: 5000 });
+    } catch (error) {
+      console.log('知乎API调用出错，请检查网络');
+    }
+    return response
+  }
+
   async getAnswers(limit, offset) {
     let uri = `https://www.zhihu.com/api/v4/questions/${this.questionId}/answers?limit=${limit}&offset=${offset}&include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&sort_by=default`;
     let response = {};
     try {
-      const { paging, data } = await rp({ uri, json: true, timeout: 10000 });
+      const { paging, data } = await rp({ uri, json: true, timeout: 5000 });
       const { is_end: isEnd, next } = paging;
       const { question } = Object(data[0]);
       const content = data.reduce((content, it) => content + it.content, '');
--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const crawler = require('./crawler');
 const express = require('express');
 const app = express();
 
+app.get('/sisterTitle', async (req, res) => {
+  try {
+    const questionId = req.query.questionId;
+    let zhihuCrawler = new crawler(questionId);
+    let result = await zhihuCrawler.getQuestion();
+    (result['type'] === 'question' && result['id'] > 0) ? res.send(result) : res.send({'code':404,'log':'没有找到对应的问题'});
+  }
+  catch(error) {
+    res.send({'code':500,'log':'调用api出错，请检查参数是否正确或者网络连接是否正常'});
+  }
+})
+
 app.get('/sisterImg', async (req, res) => {
-    let questionId = req.query.questionId;
+  try {
+    const {questionId,offset,limit} = req.query
     let zhihuCrawler = new crawler(questionId);
     // init(offset,limit);
-    let result = await zhihuCrawler.init(0,100);
+    let result = await zhihuCrawler.init(offset,limit);
     res.send(result);
+  }
+  catch(error) {
+    res.send({'code':500,'log':'调用api出错，请检查参数是否正确或者网络连接是否正常'});
+  }
 })
 
 let server = app.listen(8089, function () {
