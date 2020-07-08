fca277627bd1d4b50d99d567c61c816cb94d51f7
n8659150 <jackli900421@hotmail.com>
2020-07-08 22:23:50 +0800

    更新nodejs版本爬虫

--- nodejs_ver/crawler.js
+++ nodejs_ver/crawler.js
@@ -1,48 +1,62 @@
-let fs = require('fs');
-let rp = require('request-promise');
+const rp = require("request-promise");
 class zhihuCrawler {
-  
-  constructor(questionId) {
-    this.isEnd = false;
-    this.questionId = questionId;
-  }
-
-  async init(offset, limit) {
-    if (this.isEnd) {
-      console.log('到底了');
-      return
+    constructor(questionId) {
+        this.isEnd = false;
+        this.questionId = questionId;
     }
-    let { isEnd, uri, imgs, question } = await this.getAnswers(limit, offset);
-    this.isEnd = isEnd;
-    this.uri = uri;
-    this.imgs = imgs;
-    this.question = question;
-    return { 'meta': question, 'imgSrc': imgs, 'total': imgs.length }
-  }
 
-  async getAnswers(limit, offset) {
-    let uri = `https://www.zhihu.com/api/v4/questions/${this.questionId}/answers?limit=${limit}&offset=${offset}&include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&sort_by=default`;
-    let response = {};
-    try {
-      const { paging, data } = await rp({ uri, json: true, timeout: 10000 });
-      const { is_end: isEnd, next } = paging;
-      const { question } = Object(data[0]);
-      const content = data.reduce((content, it) => content + it.content, '');
-      const imgs = this.matchImg(content);
-      response = { isEnd, uri: next, imgs, question }
-    } catch (error) {
-      console.log('知乎API调用出错，请检查网络');
-      console.log(error);
+    async init(offset = 0, limit = 20) {
+        if (this.isEnd) {
+            console.log("到底了");
+            return;
+        }
+        const { isEnd, uri, imgs, question } = await this.getAnswers(
+            limit,
+            offset
+        );
+        this.isEnd = isEnd;
+        this.uri = uri;
+        this.imgs = imgs;
+        this.question = question;
+        return { meta: question, imgSrc: imgs, total: imgs.length };
     }
-    return response
-  }
 
-  matchImg(content) {
-    let imgs = [];
-    let matchImgOriginRe = /<img.*?data-original="(.*?)"/g;
-    content.replace(matchImgOriginRe, ($0, $1) => imgs.push($1));
-    return [...new Set(imgs)]
-  }
+    async getQuestion() {
+        const uri = `https://www.zhihu.com/api/v4/questions/${this.questionId}`;
+        try {
+          return await rp({ uri, json: true, timeout: 5000 });
+        } catch (error) {
+            console.log("知乎API调用出错，请检查网络");
+        }
+    }
 
+    async getAnswers(limit, offset) {
+        const uri = `https://www.zhihu.com/api/v4/questions/${this.questionId}/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=${limit}&offset=${offset}&sort_by=default`;
+        try {
+            const { paging, data } = await rp({
+                uri,
+                json: true,
+                timeout: 5000,
+            });
+            const { is_end: isEnd, next } = paging;
+            const { question } = Object(data[0]);
+            const content = data.reduce(
+                (content, it) => content + it.content,
+                ""
+            );
+            const imgs = this.matchImg(content);
+            return { isEnd, uri: next, imgs, question };
+        } catch (error) {
+            console.log("知乎API调用出错，请检查网络");
+            console.log(error);
+        }
+    }
+
+    matchImg(content) {
+        const imgs = [];
+        const matchImgOriginRe = /<img.*?data-original="(.*?)"/g;
+        content.replace(matchImgOriginRe, ($0, $1) => imgs.push($1));
+        return [...new Set(imgs)];
+    }
 }
 module.exports = zhihuCrawler;
--- nodejs_ver/index.js
+++ nodejs_ver/index.js
@@ -1,17 +1,42 @@
-const crawler = require('./crawler');
-const express = require('express');
+const crawler = require("./crawler");
+const express = require("express");
 const app = express();
+const allowCrossDomain = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+};
+app.use(allowCrossDomain);
 
-app.get('/sisterImg', async (req, res) => {
-    let questionId = req.query.questionId;
-    let zhihuCrawler = new crawler(questionId);
-    // init(offset,limit);
-    let result = await zhihuCrawler.init(0,100);
-    res.send(result);
-})
+app.get("/sisterTitle", async (req, res) => {
+    try {
+        const questionId = req.query.questionId;
+        const zhihuCrawler = new crawler(questionId);
+        const result = await zhihuCrawler.getQuestion();
+        result["type"] === "question" && result["id"] > 0
+            ? res.send(result)
+            : res.send({ code: 404, log: "没有找到对应的问题" });
+    } catch (error) {
+        res.send({
+            code: 500,
+            log: "调用api出错，请检查参数是否正确或者网络连接是否正常",
+        });
+    }
+});
 
-let server = app.listen(8089, function () {
+app.get("/sisterImg", async (req, res) => {
+    try {
+        const { questionId, offset, limit } = req.query;
+        const zhihuCrawler = new crawler(questionId);
+        const result = await zhihuCrawler.init(offset, limit);
+        res.send(result);
+    } catch (error) {
+        res.send({
+            code: 500,
+            log: "调用api出错，请检查参数是否正确或者网络连接是否正常",
+        });
+    }
+});
 
-  console.log('服务器在8089端口启动');
-
-})
+app.listen(8089, () => {
+    console.log("服务器在8089端口启动");
+});
